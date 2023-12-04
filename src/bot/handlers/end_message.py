67f09b44ef69b4849575e856eabe2dca2bd2a95a
Vasiliynemix <vasyanemix@gmail.com>
2023-12-04 16:02:55 +0300

    init123456789

--- src/bot/handlers/end_message.py
+++ src/bot/handlers/end_message.py
@@ -1,5 +1,6 @@
 from aiogram import Router, types
 
+from src.bot.structures.lexicon.lexicon_ru import MESSAGE_2
 from src.db.database import Database
 
 router = Router()
@@ -9,3 +10,9 @@ router = Router()
 async def funnel_zero(message: types.Message, db: Database):
     if not await db.user.check_state(user_id=message.from_user.id, state=-1):
         await message.answer(text="Нажми на кнопку")
+    else:
+        await message.answer(
+            f"Вы у нас уже были. Не оплатили? Вот ссылка: {MESSAGE_2[20:]}",
+            reply_markup=None,
+            parse_mode="HTML",
+        )
--- src/bot/handlers/start.py
+++ src/bot/handlers/start.py
@@ -3,6 +3,7 @@ from aiogram.filters import CommandStart
 
 from src.bot.filters.register import RegisterFilter
 from src.bot.handlers.funks import answer_message_1
+from src.bot.structures.lexicon.lexicon_ru import MESSAGE_2
 from src.db.database import Database
 
 router = Router()
@@ -11,4 +12,11 @@ router = Router()
 @router.message(CommandStart())
 @router.message(CommandStart(), RegisterFilter())
 async def start_handler(message: types.Message, db: Database):
-    await answer_message_1(bot=message.bot, user_id=message.from_user.id, db=db)
+    if not await db.user.check_state(user_id=message.from_user.id, state=-1):
+        await answer_message_1(bot=message.bot, user_id=message.from_user.id, db=db)
+    else:
+        await message.answer(
+            f"Вы у нас уже были. Не оплатили? Вот ссылка: {MESSAGE_2[20:]}",
+            reply_markup=None,
+            parse_mode="HTML",
+        )
